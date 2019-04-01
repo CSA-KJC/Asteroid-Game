9447eb328e447432de9c67e095425a07c43494c7
CSA-KJC <33467695+CSA-KJC@users.noreply.github.com>
2019-04-01 08:42:13 -0500

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,3 +1,21 @@
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+#####
+Katie Chiu
+Paddle
+Version 1.0
+Last updated 27 March 2019
+Shoot as many asteroids as you can with 3 lives. Each asteroid is 100 points. Each asteroid not hit is -100 points. Each potion that passes by is an extra life. Use arrows to move left and right. Use spacebar to shoot lasers.
+'''
 import pygame, random, time
 from tkinter import *
 
